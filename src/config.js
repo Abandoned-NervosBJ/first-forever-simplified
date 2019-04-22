ca7d54b6452c46cda671cfb547f8afd88d5507f7
shooter <shooter.hero@gmail.com>
2019-04-23 01:01:24 +0800

    2

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
   chain: 'https://node.cryptape.com',
-  contractAddress: '0x4E35a5B086393423D3eeC435Da213084D851d139',
-  fromAddress: '0xD3E3f3678cB868f92248791151F211AB8835A153',
-  privateKey: '0x770d4236db6fbd81cd3baf3cbdf56859fc7635515df0ea7a177d72073a38a7ac',
+  contractAddress: '0x6659fD46360cD2fCC599FD42494eBAdC95561ea6',
+  fromAddress: '0x529Aa60662689FA211AB64C723Df65122684B99B',
+  privateKey: '0xa8c0c056713dde623cc96b3e3b5850c7bba43931728a8aea04104aa90eb8c3fa',
 }
 
 module.exports = config
